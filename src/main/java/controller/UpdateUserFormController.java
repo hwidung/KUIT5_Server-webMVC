7ba24df4e777663a549b3e329ccf7f86332f7f22
hwidung <hwhwhw74@naver.com>
2025-04-11 23:42:14 +0900

    feat: 회원 목록 개인 정보 수정 보안 강화

--- src/main/java/controller/UpdateUserFormController.java
+++ src/main/java/controller/UpdateUserFormController.java
@@ -7,15 +7,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jwp.model.User;
 
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
 public class UpdateUserFormController extends HttpServlet {
+    private static final String USER_SESSION_KEY = "user";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User sessionUser = (User) session.getAttribute(USER_SESSION_KEY);
+        if (sessionUser == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
         String userId = req.getParameter("userId");
+        if (!sessionUser.isSameUser(userId)) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "본인의 정보만 수정할 수 있습니다."); // 403 에러 반환
+            return;
+        }
         User user = MemoryUserRepository.getInstance().findUserById(userId);
         req.setAttribute("user", user);
 
